3dd692c1135b5a9a7cc9bd3f807bb37c9c296c8b
Marc Davies <marcdaviesriot@gmail.com>
2020-04-19 23:17:20 -0700

    change use to most recent versions

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use] 
+extern crate rocket;
+extern crate rocket_contrib;
+extern crate serde;
+
+use rocket_contrib::templates::Template;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct IndexContext {
+    header: String,
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    let context = IndexContext {
+        header: "Hello!".to_string(),
+    };
+    Template::render("index", &context)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
+        .launch();
 }
